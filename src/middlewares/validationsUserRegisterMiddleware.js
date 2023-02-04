5182aa027e5a9d3d560c72fe97b672b7f581aa31
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2023-02-04 01:56:53 -0300

    Showing error message if age field is not a number

--- src/middlewares/validationsUserRegisterMiddleware.js
+++ src/middlewares/validationsUserRegisterMiddleware.js
@@ -4,7 +4,8 @@ const validations = [
   check("name").notEmpty().withMessage("O campo nome não pode estar vazio"),
   check("color").notEmpty().withMessage("O campo cor não pode estar vazio"),
   check("email").notEmpty().withMessage("O campo email não pode estar vazio"),
-  check("age").notEmpty().withMessage("O campo idade não pode estar vazio")
+  check("age").notEmpty().withMessage("O campo idade não pode estar vazio").bail()
+    .isNumeric().withMessage("O campo idade só aceita números")
 ];
 
 module.exports = validations;
